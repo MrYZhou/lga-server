f2becde90e0c0f23c4634411b8242d637b77e706
larry <1762861715@qq.com>
2024-12-20 15:53:51 +0800

    修改目录结构

--- util/scheduler.py
+++ util/scheduler.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.date import DateTrigger
 from apscheduler.triggers.interval import IntervalTrigger
-from server.task.model.info import TaskInfo
+from router.task.model.info import TaskInfo
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 
@@ -22,14 +22,14 @@ class Scheduler:
     def init(app: FastAPI, *args, **kwargs):
         app.add_event_handler("startup", Scheduler.startup)
         app.add_event_handler("shutdown", Scheduler.shutdown)
-    def add(task):
-        type = task.get("type")
-        task_id = task.get("id")
-        task_name = task.get("task_name")
-        content = task.get("content")
+    def add(task:TaskInfo):
+        type = task.type
+        task_id = task.id
+        task_name = task.task_name
+        content = task.content
         method =  Scheduler.create_function_from_string(content)
         if "date" == type:
-            execute_time: datetime = task.get("execute_time")
+            execute_time: datetime = task.execute_time
             current_time: datetime = datetime.now()
             if execute_time > current_time:
                 Scheduler.scheduler.add_job(
@@ -41,7 +41,7 @@ class Scheduler:
                     args=[],
                 )
         elif "cron" == type:
-            job_args = Scheduler.parse_cron_expression(task.get("cron"))
+            job_args = Scheduler.parse_cron_expression(task.cron)
             Scheduler.scheduler.add_job(
                 id=task_id,
                 name=task_name,
@@ -55,7 +55,7 @@ class Scheduler:
                 id=task_id,
                 name=task_name,
                 func=method,
-                trigger=IntervalTrigger(seconds=task.get("seconds")),
+                trigger=IntervalTrigger(seconds=task.seconds),
                 replace_existing=True,
                 args=[],
             )
