79dbecd9557809c30c2fde4612466c8a396e42f4
larry <1762861715@qq.com>
2024-12-26 09:11:36 +0800

    1

--- build.py
+++ build.py
@@ -1,14 +1,14 @@
 from PyInstaller import __main__ as pyi
 
 # 打包快捷设置,避免手动输入命令
-
 params = [
     "-F",  #  是否单文件
     # "-w",  # 是否隐藏控制台-w代表无,默认有
     "--hidden-import=nanoid",
+    "--hidden-import=fastapi.templating",
     # 额外目录纳入打包
-    "--add-data=util;util",
     "--add-data=router;router",
+    "--add-data=util;util",
     "--add-data=tasks;tasks",
     # 指定输出目录
     "--distpath=build",
