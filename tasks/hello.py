60fd499cf297d32371523512a84d22693f62a8dc
larry <1762861715@qq.com>
2025-01-03 13:44:34 +0800

    1

--- tasks/hello.py
+++ tasks/hello.py
@@ -7,10 +7,11 @@ class Config:
     task_name='任务1'
 url = "https://juejin.cn/user/center/signin?from=main_page"
 def main():
+    print('hello')
     # 启动或接管浏览器，并创建标签页对象
-    co = ChromiumOptions().headless()
+    # co = ChromiumOptions().headless()
 
-    tab = Chromium(9224,co).latest_tab
-    # 跳转到目标页面
-    tab.get(url)
+    # tab = Chromium(9224,co).latest_tab
+    # # 跳转到目标页面
+    # tab.get(url)
     
