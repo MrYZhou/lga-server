821edb74309de90717227cf437f36453dfdfc93c
larry <1762861715@qq.com>
2025-01-06 09:04:06 +0800

    1

---
+++ tasks/hello2.py
@@ -0,0 +1,17 @@
+from DrissionPage import Chromium
+from DrissionPage._configs.chromium_options import ChromiumOptions
+from util.system import  Env
+class Config:
+    type = 'interval'
+    seconds = 1
+    task_name='任务1'
+url = "https://juejin.cn/user/center/signin?from=main_page"
+def main():
+    print('hello2')
+    # 启动或接管浏览器，并创建标签页对象
+    # co = ChromiumOptions().headless()
+
+    # tab = Chromium(9224,co).latest_tab
+    # # 跳转到目标页面
+    # tab.get(url)
+    
